294d27d255795c33447186f5509a25553888bff5
promoted.go@daamdaam.co.kr <promoted.go@daamdaam.co.kr>
2021-06-18 16:49:02 +0900

    wow

--- lotte-Client/MainWindow.xaml.cs
+++ lotte-Client/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace lotte_Client
         public MainWindow()
         {
             InitializeComponent();
-            MessageBox.Show("Hello world")!;
+            //wow
         }
     }
 }
